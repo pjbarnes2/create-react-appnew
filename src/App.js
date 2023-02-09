80ad92a88064d6bb3a3219e6fa8df3e573199308
pjbarnes2 <pjb5805@psu.edu>
2023-02-09 13:03:46 -0500

    react update

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+  <div class="buttons">
+    <button class="dupe">Duplicate!! Create army of Toph!</button>
+    <button class="changecolor">Change Color</button>
+    <button class="newtitle">New Title</button>
+    <button class="delete">Delete</button>
+    <button class="data-toggle-btn">Toggle Button</button>
+  </div>
+  <div class="wrapper">
+    <div class="container">
+      <img class="image" src="https://i.kym-cdn.com/entries/icons/mobile/000/043/203/tophercover.jpg" />
+
+      <div class="header">
+        <h3>Topher</h3>
+        <h4>God king of the Universe</h4>
+      </div>
+      <div class="details">
+        <summary>Topher Facts</summary>
+        <div>
+          <ul>
+            <li>killed god and took his place</li>
+            <li>owns 70% of all rae dunn mugs</li>
+            <li>favorite color is teal</li>
+            <li>favorite store is tj maxx</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+      </div>
+</div>
+);
 }
 
 export default App;
